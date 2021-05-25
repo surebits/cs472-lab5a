158dea05b965295b3b0d7732bfd3bae73a9b65f1
surebits <breadphace@gmail.com>
2021-05-25 18:50:32 +0100

    More fixea

---
+++ bugyy1.js
@@ -0,0 +1,15 @@
+function gogogo() {
+    alert("Yay, it works!");
+}
+/*
+ Original code 
+ function gogogo(); {
+    Alert("Yay, it works!''); 
+    
+    1. keywords are case sensitive so change Alert for alert
+    2. The alert parameter string is not properly formatted
+
+) 3. this should be a closing brace 
+
+    see correction above
+*/
